be85d97d436e927a583f38e58d2afd19cbe947f9
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-05-19 23:29:41 +0530

    Create all arithmetic operator.c

---
+++ all arithmetic operator.c
@@ -0,0 +1,51 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int x=2+3;
+printf("%d",x);
+  
+  // Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int x=2-4;
+printf("%d",x);
+  
+  // Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int x=2/3;
+printf("%d",x);
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int x=6/3;
+printf("%d",x);
+    return 0;
+  
+  
+ // Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int x=23%3;
+printf("%d",x);
+    return 0;
+
+}
+
+
+
+    
+    
+
+
+
+
+
+    
+    
