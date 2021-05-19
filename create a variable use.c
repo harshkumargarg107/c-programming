e641eb6cf0457f2b122169a88d3ee1acc0ec38e1
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-05-19 22:09:37 +0530

    Create create a variable use.c

---
+++ create a variable use.c
@@ -0,0 +1,11 @@
+int main() {
+    // Write C code here
+    int x;
+    int y;
+    x=2;
+    y=3;
+    
+    printf("the value of variable x is %d \n" ,x);
+    printf("the value of variablr y is %d",y);
+    return 0;
+}
