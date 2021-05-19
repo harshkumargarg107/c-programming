43f71c87f6249342686039f7c1bca807c2a884bb
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-05-19 23:00:57 +0530

    Create size of different data size.c

---
+++ size of different data size.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+int x,y,z;
+x=sizeof('45');
+y=sizeof('4.6');
+z=sizeof('a');
+printf("%d %d %d",x,y,z);
+    
+    return 0;
+}
