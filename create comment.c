964d2275cee9b387b0b649ed18483e7a2e6dbb41
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-05-19 21:58:24 +0530

    Create create comment.c

---
+++ create comment.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("harsh kumar garg i belong to sikrai Mehandipur Balaji district dausa rajasthan");
+    
+    return 0;
+}
