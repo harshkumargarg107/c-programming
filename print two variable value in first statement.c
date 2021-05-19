6ad15d70e717367ae3d2cc2d4583bb0e4994eed3
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-05-19 22:20:13 +0530

    Create print two variable value in first statement.c

---
+++ print two variable value in first statement.c
@@ -0,0 +1,11 @@
+int main() {
+    // Write C code here
+    int x;
+    int y;
+    x=4;
+    y=35;
+    
+    printf("the value of variable x is %d \n" ,x);
+    printf("the value of variablr y is %d",y);
+    return 0;
+}
