5a87b0a3f5f5a7043391afc64aa0f68225ca78e7
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-05-20 11:02:16 +0530

    Create use the constant.c

---
+++ use the constant.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   const pi =3.14;
+   
+   
+    printf("value of pi %d",pi);
+    
+    return 0;
+}
