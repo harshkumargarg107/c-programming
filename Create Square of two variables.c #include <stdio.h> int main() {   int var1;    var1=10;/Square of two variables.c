391bbf07ec2148b95a0d2e13ce63a640e56552c1
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-07-27 21:03:56 +0530

    Create Square of two variables.c

---
+++ Create Square of two variables.c #include <stdio.h> int main() {   int var1;    var1=10;/Square of two variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+  int var1;
+   var1=10;
+    // Here, we will take an example.
+    printf("Square of integer variable: %i \n", var1*var1 );
+
+float var2;
+   var2=10.5;
+    // Here, we will take an example.
+    printf("Square of float variable: %f \n", var2*var2 );
+
+char var3;
+   var3='A';
+    // Here, we will take an example.
+    printf("The available chartacter is: %c \n", var3 );
+
+double var4;
+   var4=11.23;
+    // Here, we will take an example.
+    printf("Square of double variable: %lf \n", var4*var4 );
+
+    return 0;
