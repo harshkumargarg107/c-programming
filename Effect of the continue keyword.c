9f66ed5018e6306a3219624a10bb3200a5db5e24
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-07-27 20:42:29 +0530

    Create Effect of the continue keyword.c

---
+++ Effect of the continue keyword.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// loop from 1 to 10
+	for (int i = 1; i <= 10; i++) {
+
+		// If i is equals to 6,
+		// continue to next iteration
+		// without printing
+		if (i == 6)
+			continue;
+
+		else
+			// otherwise print the value of i
+			printf("%d", i);
+			
+			printf("%d ", i);
+	}
