791cb5f21b595da9cb55508407adfc09c8a7afde
harshkumargarg107 <84460917+harshkumargarg107@users.noreply.github.com>
2021-07-27 20:43:37 +0530

    Create while and do while.c

---
+++ while and do while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n=10, a=1, b=1;
+    
+    while(a<n){
+        printf("%d\n",a);
+        a+=2;
+    }
+    
+    printf("now we see do while loop\n");
+    
+    do {printf("%d\n",b);
+        b+=2;}while(b<n);
+    
+    return 0;
+}
